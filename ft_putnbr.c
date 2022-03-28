321b4e790bf3ffaa706b5824b50d504b5073d29e
Valentin Simeonov <vsimeono@c2s4d11.42wolfsburg.de>
2022-03-28 17:18:56 +0200

    First Exam Function

---
+++ ft_putnbr.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+
+
+
+void	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
+/* Recursive putnbr */
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	if (nb < 10)
+		ft_putchar(nb + 48);
+}
+
+int	main()
+{
+	int		num;
+
+	num = 23;
+	ft_putnbr(num);
+	return (0);
+}
