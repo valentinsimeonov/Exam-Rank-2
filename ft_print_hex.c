a4c50faf203f4548a62baedbaa55040c35549c66
Valentin Simeonov <vsimeono@c2s4d11.42wolfsburg.de>
2022-04-03 19:45:44 +0200

    Added print_hex

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -19,14 +19,47 @@ $> ./print_hex "5156454" | cat -e
 4eae66$
 $> ./print_hex | cat -e
 $
-
 */
 
 #include <unistd.h>
 
-void	ft_putchar(int num)
+void	ft_putchar(char c)
 {
-	write(1, &num, 1);
+	write(1, &c, 1);
 }
 
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		result;
 
+	i = 0;
+	result = 0;
+	while (str[i] != '\0')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result);
+}
+
+void	print_hex(int nb)
+{
+	if (nb >= 16)
+	{
+		print_hex(nb / 16);
+		print_hex(nb % 16);
+	}
+	if (nb < 10)
+		ft_putchar(nb + 48);
+	else if (nb < 16)
+		ft_putchar(nb + 87);
+}
+
+int		main (int argc, char **argv)
+{
+	if (argc == 2)
+		print_hex(ft_atoi(argv[1]));
+	write(1, "\n", 1);
+	return (0);
+}
