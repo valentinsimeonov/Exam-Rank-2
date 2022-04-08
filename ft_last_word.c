bd7e8b567ffce1a01fa2aaeb66128f581786827c
Valentin Simeonov <vsimeono@c2s4d11.42wolfsburg.de>
2022-04-08 17:52:15 +0200

    Added ft_last_word

---
+++ ft_last_word.c
@@ -0,0 +1,55 @@
+/*
+Assignment name  : last_word
+Expected files   : last_word.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays its last word followed by a \n.
+
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string.
+
+If the number of parameters is not 1, or there are no words, display a newline.
+
+Example:
+
+$> ./last_word "FOR PONY" | cat -e
+PONY$
+$> ./last_word "this        ...       is sparta, then again, maybe    not" | cat -e
+not$
+$> ./last_word "   " | cat -e
+$
+$> ./last_word "a" "b" | cat -e
+$
+$> ./last_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>
+*/
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		start;
+
+	i = 0;
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	while (argv[1][i] != '\0')
+	{
+		if (argv[1][i] == ' ' && argv[1][i + 1] != ' ' && argv[1][i + 1] != '\0')
+			start = i + 1;
+		i++;
+	}
+	while (argv[1][start] != ' ' && argv[1][start] != '\0')
+	{
+		write(1, &argv[1][start], 1);
+		start++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
