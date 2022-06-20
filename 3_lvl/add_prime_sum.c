464935ab812e3c251e4049748eae3c725a47e069
Valentin Simeonov <vsimeono@c2s19d120.42wolfsburg.de>
2022-06-20 19:24:20 +0200

    Added Level 3

---
+++ 3_lvl/add_prime_sum.c
@@ -0,0 +1,124 @@
+/*
+Assignment name  : add_prime_sum
+Expected files   : add_prime_sum.c
+Allowed functions: write, exit
+--------------------------------------------------------------------------------
+
+Write a program that takes a positive integer as argument and displays the sum
+of all prime numbers inferior or equal to it followed by a newline.
+
+If the number of arguments is not 1, or the argument is not a positive number,
+just display 0 followed by a newline.
+
+Yes, the examples are right.
+
+Examples:
+
+$>./add_prime_sum 5
+10
+$>./add_prime_sum 7 | cat -e
+17$
+$>./add_prime_sum | cat -e
+0$
+$>
+
+2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+
+*/
+
+
+#include <unistd.h>
+
+#include <unistd.h>
+
+int		ft_atoi(char *str)
+{
+	int		i;
+	int		num;
+	int		sign;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' && str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = sign * (-1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (num * 10) + (str[i] - '0');
+		i++;
+	}
+	return (num * sign);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int num)
+{
+	int		i;
+	int		sign;
+
+	i = 0;
+	sign = 1;
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		num = num * (-1);
+	}
+	if (num >= 10)
+	{
+		ft_putnbr(num / 10);
+		num = num % 10;
+	}
+	if (num < 10)
+	{
+		ft_putchar(num + '0');
+	}
+}
+
+int		is_prime(int n)
+{
+	int i = 2;
+
+	while (i < n)
+	{
+		if (n % i == 0)
+			return (0);
+		++i;
+	}
+	return (1);
+}
+
+int		add_prime_sum(int n)
+{
+	int sum = 0;
+	int i = 2;
+
+	while (i <= n)
+	{
+		if (is_prime(i) == 1)
+			sum += i;
+		++i;
+	}
+	return (sum);
+}
+
+int		main(int argc, char **argv)
+{
+	int n;
+
+	if (argc == 2 && (n = ft_atoi(argv[1])))
+		ft_putnbr(add_prime_sum(n));
+	else
+		ft_putnbr(0);
+	write(1, "\n", 1);
+	return (0);
+}
